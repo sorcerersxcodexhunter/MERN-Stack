b307093689dc0940c1ddc244367eb9d17e1e5272
Rahul Patel <145385496+sorcerersxcodexhunter@users.noreply.github.com>
2025-07-04 00:26:49 +0530

    Add files via upload

---
+++ 02-07-2025/Rahul-React/src/Components/button.jsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Btn = () => {
+  return (
+    <button className="btn text-white bg-primary m-2">
+      Click Me
+    </button>
+  )
+}
+export default Btn;
