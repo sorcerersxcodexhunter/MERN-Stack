a3ed02ec74857bb8997dc7ec55b14882afa90204
Rahul Patel <145385496+sorcerersxcodexhunter@users.noreply.github.com>
2025-07-22 01:20:50 +0530

    Add files via upload

---
+++ 03-07-2025/src/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from './Component/Form'
+
+function Register() {
+  return (
+    <div>
+        <h1>Register</h1>
+      <Form submitlabel='Register' navigateTo='/' />
+    </div>
+  )
+}
+
+export default Register
